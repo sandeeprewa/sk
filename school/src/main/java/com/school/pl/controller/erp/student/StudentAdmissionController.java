41328395aea982789808743eaf59e46921cb036b
sandeep pandey <sp.sendmail@gmail.com>
2017-04-01 14:32:43 +0530

    adding api in student admission controller

--- school/src/main/java/com/school/pl/controller/erp/student/StudentAdmissionController.java
+++ school/src/main/java/com/school/pl/controller/erp/student/StudentAdmissionController.java
@@ -37,7 +37,8 @@ public class StudentAdmissionController {
 
 
 	@RequestMapping(value = "{studentId}", method = RequestMethod.GET)
-	public StudentAdmissionRecord getStudentAdmissionRecord(@PathVariable String studentId) throws StudentIdDoesNotExistException {
+	public StudentAdmissionRecord getStudentAdmissionRecord(@PathVariable String studentId) 
+			throws StudentIdDoesNotExistException {
 		return studentAdmissionFacade.getAdmissionRecordByStudentId(studentId);
 	}
 	
@@ -48,7 +49,8 @@ public class StudentAdmissionController {
 	}
 	
 	@RequestMapping(value = "{studentId}" ,method = RequestMethod.PUT)
-	public StudentAdmissionRecord updateAdmissionStudentRecord(@PathVariable String studentId, @Validated @RequestBody StudentAdmissionRecord admissionRecord)
+	public StudentAdmissionRecord updateAdmissionStudentRecord(@PathVariable String studentId, 
+			@Validated @RequestBody StudentAdmissionRecord admissionRecord)
 	throws StudentIDEditException{
 		return studentAdmissionFacade.updateStudentAdmissionRecord(admissionRecord);
 	}
@@ -63,12 +65,14 @@ public class StudentAdmissionController {
 	 * Better way it can be implmented
 	 */
 	@RequestMapping(value = "fname/{studentFirstName}", method = RequestMethod.GET)
-	public List<StudentAdmissionRecord> getStudentAdmissionRecordByStudentName(@PathVariable String studentFirstName) throws StudentDoesNotExistException {
+	public List<StudentAdmissionRecord> getStudentAdmissionRecordByStudentName(@PathVariable String studentFirstName) 
+			throws StudentDoesNotExistException {
 		return studentAdmissionFacade.getAdmissionRecordByStudentFirstName(studentFirstName);
 	}
 	
 	@RequestMapping(value = "lname/{studentLastName}", method = RequestMethod.GET)
-	public List<StudentAdmissionRecord> getStudentAdmissionRecordByStudentLastName(@PathVariable String studentLastName)throws StudentDoesNotExistException {
+	public List<StudentAdmissionRecord> getStudentAdmissionRecordByStudentLastName(@PathVariable String studentLastName)
+			throws StudentDoesNotExistException {
 		return studentAdmissionFacade.getAdmissionRecordByStudentLastName(studentLastName);
 	}
 
