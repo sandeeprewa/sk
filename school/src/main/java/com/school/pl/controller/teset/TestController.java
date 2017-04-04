bb3ef3daacf18ddd76aabf509296eea70bafe0d8
sandeep pandey <sp.sendmail@gmail.com>
2017-04-04 13:15:16 +0530

    sample test case

--- school/src/main/java/com/school/pl/controller/teset/TestController.java
+++ school/src/main/java/com/school/pl/controller/teset/TestController.java
@@ -5,8 +5,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +24,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 public class TestController {
 
+	@Autowired
+	HttpServletRequest  request;
 	
 	@RequestMapping(value = "/test/post", method = RequestMethod.POST)
 	public String handle(@RequestBody World world){
@@ -53,22 +57,20 @@ public class TestController {
 	public String tese1t(@Valid @ModelAttribute Pic login) throws JsonParseException, JsonMappingException, IOException{
 		System.out.println(login.getName());
 		MultipartFile multipartFile = login.getFile1();
-		saveIntoLocalServerDirectory(multipartFile);	
+		transfer(multipartFile);
 		ComplexObjectImage complexObject = new ObjectMapper().readValue(login.getName(), ComplexObjectImage.class);
 		System.out.println(complexObject.getCountry().getCountryName());
 		System.out.println(complexObject.getQuality().toString());
 		return "hii";
 	}
 	
-	@SuppressWarnings("unused")
+/*	@SuppressWarnings("unused")
 	private String saveIntoLocalServerDirectory(MultipartFile multipartFile) throws IOException{
 		String ROOT_PATH = System.getProperty("catalina.home");
 		byte[] bytes = multipartFile.getBytes();
+		
 		String name = multipartFile.getName();
 		System.out.println( name);
-		File dir = new File(ROOT_PATH + File.separator + "tmpFilesOK");
-		if (!dir.exists())
-		     dir.mkdirs();
 	
 		// Create the file on server
 		File serverFile = new File(dir.getAbsolutePath()+ File.separator + name);
@@ -77,5 +79,33 @@ public class TestController {
 		stream.close();
 		return serverFile.getAbsolutePath();
 	}
+*/
+	private void transfer(MultipartFile multipartFile){
+		String ROOT_PATH = System.getProperty("catalina.home");
+		File dir = new File(ROOT_PATH + File.separator + "uploads");
+		if (!dir.exists())
+		     dir.mkdirs();
 
+		try {
+			 String uploadsDir = "/upload/";
+             String realPathtoUploads =  request.getServletContext().getRealPath(dir.getName());
+             System.out.println(realPathtoUploads);
+             System.out.println(request.getServletContext().getRealPath(uploadsDir));
+             if(! new File(realPathtoUploads).exists())
+             {
+                 new File(realPathtoUploads).mkdir();
+             }
+
+             String orgName = multipartFile.getOriginalFilename();
+             String filePath = realPathtoUploads + orgName;
+             File dest = new File(filePath);
+             multipartFile.transferTo(dest);
+		}catch(Exception e){
+			
+		}
+		
+	}
+	
+	
+	
 }
