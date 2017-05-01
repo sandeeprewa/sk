37b2e086462ad10cf8ee4a75bab018d4e8a3aaa5
dhananjay <dhananjaynamdeo07@gmail.com>
2017-05-01 09:38:46 +0530

    updating the files

--- school/src/main/java/com/academics/school/pl/controller/admission/StudentAdmissionController.java
+++ school/src/main/java/com/academics/school/pl/controller/admission/StudentAdmissionController.java
@@ -1,5 +1,148 @@
 package com.academics.school.pl.controller.admission;
 
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.academics.school.pl.controller.registration.dto.FakeStudentRegistrationDTO;
+import com.academics.school.pl.controller.registration.dto.RegistrationRecordStatusTrackerDTO;
+import com.academics.school.pl.controller.registration.dto.SearchRegistrationRequestDTO;
+import com.academics.school.pl.controller.registration.dto.StudentRegistrationRecord;
+import com.academics.school.pl.controller.registration.error.RegistrationRecordDoesNotExistException;
+import com.academics.school.pl.controller.registration.error.StudentAlreadyRegisteredException;
+import com.academics.school.pl.controller.registration.error.StudentIDEditException;
+import com.academics.school.pl.controller.registration.error.StudentIdDoesNotExistException;
+import com.academics.school.pl.controller.registration.error.StudentRegistrationFieldValidationException;
+import com.academics.school.pl.controller.registration.validation.RegistrationRecordStatusDTOValidator;
+import com.academics.school.pl.controller.registration.validation.StudentRegisterationRecordValidator;
+import com.academics.school.pl.rest.security.PrivilageEnum;
+import com.academics.school.pl.rest.security.RequiredPrivilage;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/*
+ * @author sandeep
+ */
+@RestController
+@RequestMapping("/rest/admission/")
 public class StudentAdmissionController {
 
+	@Autowired
+	StudentAdmissionFacade studentAdmissionFacade;
+
+	/*
+	 * Create New Registration Record 
+	 */
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@RequiredPrivilage({PrivilageEnum.PUBLIC_USER})
+	public StudentRegistrationRecord createNewStudentRegistrationRecord(@RequestBody FakeStudentRegistrationDTO fakeRegistrationRecord) 
+			throws StudentRegistrationFieldValidationException, StudentAlreadyRegisteredException, JsonParseException, JsonMappingException, IOException, StudentRegistrationFieldValidationException {
+		StudentRegistrationRecord studentRegistrationRecord = buildStudentRegistrationRecordDTO(fakeRegistrationRecord);
+		validate(studentRegistrationRecord);
+		return studentAdmissionFacade.createRegistrationStudentRecord(studentRegistrationRecord);
+	}
+	
+	/*
+	 * Getting registration record by registrationID
+	 */
+	@RequestMapping(value = "{registrationId}", method = RequestMethod.GET)
+	@RequiredPrivilage({PrivilageEnum.PUBLIC_USER})
+	public StudentRegistrationRecord getStudentAdmissionRecord(@Valid @NotNull @PathVariable String registrationId) 
+			throws StudentIdDoesNotExistException {
+		return studentAdmissionFacade.getRegistrationRecordByRegistrationId(registrationId);
+	}
+	
+	/*
+	 * Updating Existing Registration Record
+	 */
+	@RequestMapping(value = "{registrationId}" ,method = RequestMethod.PUT)
+	@RequiredPrivilage({PrivilageEnum.END_USER, PrivilageEnum.EDITOR})
+	public StudentRegistrationRecord updateAdmissionStudentRecord(@PathVariable String registrationId, 
+			@Validated @RequestBody StudentRegistrationRecord admissionRecord)
+	throws StudentIDEditException{
+		return studentAdmissionFacade.updateStudentRegistrationRecord(admissionRecord);
+	}
+
+	/*
+	 * Deletion of Registration Record
+	 */
+	@RequiredPrivilage({PrivilageEnum.EDITOR})
+	@RequestMapping(value = "{registrationId}", method = RequestMethod.DELETE)
+	public StudentRegistrationRecord deleteAdmissionStudentRecord(@PathVariable String registrationId){
+		return studentAdmissionFacade.deleteStudentRegistrationRecordById(registrationId);
+	}
+	
+	/*
+	 * Search of Registration Record based on Diff Parameter
+	 */
+	@RequiredPrivilage({PrivilageEnum.PUBLIC_USER})
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public List<StudentRegistrationRecord> getStudentAdmissionRecordByStudentName(@RequestBody SearchRegistrationRequestDTO searchRegistrationRequestDTO) 
+			throws RegistrationRecordDoesNotExistException {
+		return studentAdmissionFacade.getRegistrationRecordBasedOnDiffParameter(searchRegistrationRequestDTO);
+	}
+	
+	/*
+	 * Paying Fee
+	 */
+	@RequiredPrivilage({PrivilageEnum.PUBLIC_USER})
+	@RequestMapping(value = "/{registrationId}/fee", method = RequestMethod.POST)
+	public String payFee(@PathVariable String registrationId){
+		return "Fees Paid ! Testing";
+	}
+	
+	/*
+	 * Admin Facility - Changing Status of Registration Record
+	 */
+	@RequestMapping(value = "/status/", method = RequestMethod.PUT)
+	@RequiredPrivilage({PrivilageEnum.EDITOR})
+	public List<StudentRegistrationRecord> changeRegistrationRecordStatus(@RequestBody RegistrationRecordStatusTrackerDTO[] inputStatusDTO){
+		ArrayList<RegistrationRecordStatusTrackerDTO> statusDTOList = new ArrayList<RegistrationRecordStatusTrackerDTO>(Arrays.asList(inputStatusDTO));
+		validateListOfRegistrationRecordStatusTrackerDTO(statusDTOList);
+		List<StudentRegistrationRecord> registeredRecord = studentAdmissionFacade.changeStatusOfRegistrationRecord(statusDTOList);
+		return registeredRecord;
+	}
+	
+	/*
+	 * Collecting Fee
+	 */
+	@RequestMapping(value = "/collect/{registrationId}/fee")
+	@RequiredPrivilage({PrivilageEnum.EDITOR})
+	public String collectFee(@PathVariable String registrationId){
+		return "collected";	
+	}
+	
+	private void validateListOfRegistrationRecordStatusTrackerDTO(
+			ArrayList<RegistrationRecordStatusTrackerDTO> statusDTOList) {
+		RegistrationRecordStatusDTOValidator.validate(statusDTOList);
+	}
+
+	private StudentRegistrationRecord buildStudentRegistrationRecordDTO(FakeStudentRegistrationDTO
+			   fakeRegistrationRecord) throws JsonParseException, JsonMappingException, IOException {
+			StudentRegistrationRecord studentRegistrationRecord = (StudentRegistrationRecord)new ObjectMapper()
+							.readValue(fakeRegistrationRecord.getRegistrationJson(),StudentRegistrationRecord.class);
+			studentRegistrationRecord.setStudentImage(fakeRegistrationRecord.getStudentImage());
+			studentRegistrationRecord.setBirthCertificate(fakeRegistrationRecord.getBirthCertificate());
+			studentRegistrationRecord.setCastCertificate(fakeRegistrationRecord.getCastCertificate());
+			return studentRegistrationRecord;
+	}
+
+	private void validate(StudentRegistrationRecord studentRegistrationRecord) throws StudentRegistrationFieldValidationException {
+		StudentRegisterationRecordValidator.validate(studentRegistrationRecord);
+	}
 }
--- school/src/main/java/com/academics/school/pl/controller/admission/error/handler/StudentAdmissionErrorHandler.java
+++ school/src/main/java/com/academics/school/pl/controller/admission/error/handler/StudentAdmissionErrorHandler.java
@@ -1,5 +1,107 @@
 package com.academics.school.pl.controller.admission.error.handler;
 
-public class StudentAdmissionErrorHandler {
+import java.util.Locale;
 
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.academics.school.pl.controller.admission.error.StudentAlredyAdmittedException;
+
+import com.academics.school.pl.controller.registration.error.RegistrationRecordDoesNotExistException;
+import com.academics.school.pl.controller.registration.error.StudentAlreadyRegisteredException;
+import com.academics.school.pl.controller.registration.error.StudentIDEditException;
+import com.academics.school.pl.controller.registration.error.StudentIdDoesNotExistException;
+import com.academics.school.pl.controller.registration.error.StudentRegistrationFieldValidationException;
+import com.academics.school.pl.rest.global.error.Code;
+import com.academics.school.pl.rest.global.error.FieldError;
+import com.academics.school.pl.rest.global.error.RestError;
+import com.academics.school.pl.rest.global.error.RestErrorMessage;
+
+@ControllerAdvice
+public class StudentAdmissionErrorHandler {
+	
+	private RestError restError = null;
+
+	@Autowired
+	MessageSource messageSource;
+	
+ 	/*@ResponseBody
+	@ExceptionHandler(value = StudentRegistrationFieldValidationException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public RestError handleStudentAdmissionFieldValidationException(StudentRegistrationFieldValidationException exception){
+		restError = new RestError();
+    	restError.setHttpStatus(HttpStatus.BAD_REQUEST.toString());
+		restError.setCode(Code.FEES_NOT_PAID.toString());
+		restError.setMessage(messageSource.getMessage(RestErrorMessage.VALIDATION_FAILURE,null, Locale.US));
+		restError.setFieldError(new FieldError(exception.getKey()
+				,messageSource.getMessage(exception.getMsg(),null, Locale.US)));
+		return restError;	
+	}*/
+	
+	/**
+	 * @param exception
+	 * @return
+	 */
+	@ResponseBody
+	@ExceptionHandler(value = StudentAlredyAdmittedException.class)
+	@ResponseStatus
+	public RestError handleStudentAlreadyAdmittedException(StudentAlredyAdmittedException exception){
+		restError = new RestError();
+		restError.setHttpStatus(HttpStatus.CONFLICT.toString());
+		restError.setCode(Code.FEES_NOT_PAID.toString());
+		restError.setMessage(messageSource.getMessage(RestErrorMessage.ALREADY_REGISTERED, null, Locale.US));
+		return restError;
+	}
+	
+	@ResponseBody
+	@ExceptionHandler(value = StudentIDEditException.class)
+	@ResponseStatus(code=HttpStatus.BAD_REQUEST)
+	public RestError handleStudentIDEditException(StudentIDEditException exception) {
+		restError = new RestError();
+		restError.setHttpStatus(HttpStatus.BAD_REQUEST.toString());
+		restError.setCode(Code.FEES_NOT_PAID.toString());
+		restError.setMessage(messageSource.getMessage(RestErrorMessage.UNEDITABLE_FIELD, null, Locale.US));
+		return restError;
+	}
+	
+	@ResponseBody
+	@ExceptionHandler(value = StudentIdDoesNotExistException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public RestError handleStudentIdDoesNotExistException(StudentIdDoesNotExistException exception){
+		restError = new RestError();
+		restError.setHttpStatus(HttpStatus.NOT_FOUND.toString());
+		restError.setCode(Code.FEES_NOT_PAID.toString());
+		restError.setMessage(messageSource.getMessage(RestErrorMessage.STUDENT_DOES_NOT_EXIST, null, Locale.US));
+		return restError;
+	}
+	
+	@ResponseBody
+	@ExceptionHandler(value = RegistrationRecordDoesNotExistException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public RestError handleStudentDoesNotExistException(RegistrationRecordDoesNotExistException exception){
+		restError = new RestError();
+		restError.setHttpStatus(HttpStatus.NOT_FOUND.toString());
+		restError.setCode(Code.FEES_NOT_PAID.toString());
+		restError.setMessage(messageSource.getMessage(RestErrorMessage.STUDENT_DOES_NOT_EXIST, null, Locale.US));
+		return restError;
+	}
+
+	@ResponseBody
+	@ExceptionHandler(value = ConstraintViolationException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public RestError handleConstraintViloationException(ConstraintViolationException e){
+		restError = new RestError();
+		restError.setHttpStatus(HttpStatus.BAD_REQUEST.toString());
+		restError.setCode(Code.FEES_NOT_PAID.toString());
+		restError.setMessage(messageSource.getMessage(RestErrorMessage.INPUT_IS_INVALID, null, Locale.US));
+		return restError;
+	}
+	
 }
